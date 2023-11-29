2dfa89288a97119b8e9ab5247c99f91949023e3b
KnyazevBR <145508470+KnyazevBR@users.noreply.github.com>
2023-11-29 18:58:03 +0300

    Update 4.pas

---
+++ 4.pas
@@ -0,0 +1,21 @@
+﻿var
+  d,c,a,l:string;
+  v,i:integer;
+begin
+  readln(d);
+  v:=Length(d);
+  if v > 6 then
+    begin
+    for i:=1 to 3 do
+      begin
+        write(' ', d[i])
+      end;
+    for i:=v-2 to v do
+      begin
+        write(' ', d[i])
+      end;
+    end
+  else
+    for i:=1 to v do
+      write(' ', d[1])
+end.
