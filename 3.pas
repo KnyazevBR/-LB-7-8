339b294ffaaa12034589ec5b500c54fa039f4617
KnyazevBR <145508470+KnyazevBR@users.noreply.github.com>
2023-11-29 18:57:25 +0300

    Update 3.pas

---
+++ 3.pas
@@ -0,0 +1,17 @@
+﻿var
+  d,c,i,a,l:string;
+  v:integer;
+begin
+  readln(d);
+  v:=Length(d);
+  c:= d[1];
+  a:= d[v];
+  if v mod 2 <> 0 then
+    begin
+    v:= v div 2;
+    l:= d[v+1];
+    writeln(c,l,a);
+    end
+  else
+    writeln(c,a);
+end.
