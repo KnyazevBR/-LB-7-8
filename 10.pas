bdcc248addaa1e169ff1b728d4b07e8788c766d5
KnyazevBR <145508470+KnyazevBR@users.noreply.github.com>
2023-12-04 14:13:44 +0300

    10.pas

---
+++ 10.pas
@@ -0,0 +1,15 @@
+﻿var 
+s: string;
+c: integer;
+begin
+  readln(s);
+  c:= pos('abc',s);
+if (pos('abc',s) = 1) then 
+  begin
+  Delete (s, c, 3);
+  insert('www', s, 1)
+  end
+  else
+    s:=s+'zzz';
+  writeln(s)
+end.
