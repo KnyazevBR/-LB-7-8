31a41659b168e6b72d2e566c8aa5600eec282988
KnyazevBR <145508470+KnyazevBR@users.noreply.github.com>
2023-12-04 14:15:07 +0300

    14.pas

---
+++ 14.pas
@@ -0,0 +1,13 @@
+﻿var
+  s:string;
+  c:integer;
+begin
+  readln(s);
+  while 'word' in s do
+  begin
+  c:= pos('word', s);
+  Delete (s, c, 4);
+  insert('letter', s, c);
+  end;
+  writeln(s);
+end.
