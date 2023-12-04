8e2d109eceb7203e4b5c1b26c55d4de20152d256
KnyazevBR <145508470+KnyazevBR@users.noreply.github.com>
2023-12-04 14:13:18 +0300

    1.pas

---
+++ 1.pas
@@ -0,0 +1,13 @@
+﻿var
+  s:string;
+  c:integer;
+begin
+  readln(s);
+  while 'Nikolay' in s do
+  begin
+  c:= pos('Nikolay', s);
+  Delete (s, c, 7);
+  insert('Oleg', s, c);
+  end;
+  writeln(s);
+end.
