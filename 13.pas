076674541faa6f59f5df16f754f3c18b50e2a585
KnyazevBR <145508470+KnyazevBR@users.noreply.github.com>
2023-12-04 14:14:45 +0300

    13.pas

---
+++ 13.pas
@@ -0,0 +1,18 @@
+﻿var
+  s,d:string;
+  c,i,cant:integer;
+begin
+  d:='abc';
+  readln(s);
+  c:=Length(s);
+  for i:= 1 to c do
+    begin
+    if s[i] not in d then
+      cant:=cant+1;
+    end;
+  if cant = 1 then
+    writeln('В строке содержатся не только символы "a", "b", "c" ')
+  else
+    writeln('В строке содержатся только символы "a", "b", "c" ');
+
+end.
