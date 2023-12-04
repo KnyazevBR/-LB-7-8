9dbb40787acccc9fe076ce5935446ff764f101a7
KnyazevBR <145508470+KnyazevBR@users.noreply.github.com>
2023-12-04 14:14:02 +0300

    11.pas

---
+++ 11.pas
@@ -0,0 +1,17 @@
+﻿var
+  s:string;
+  c,i:integer;
+begin
+  readln(s);
+  c:=Length(s);
+  if c > 10 then
+    begin
+    Delete (s, 7, c);
+    end
+  else
+    for i:=c to 11 do
+    begin
+      s:=s+'0';
+    end;
+  write(s);
+end.
