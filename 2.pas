0503a5aa1bd30109e90bf3a10111026ad414f217
KnyazevBR <145508470+KnyazevBR@users.noreply.github.com>
2023-12-04 14:16:20 +0300

    2.pas

---
+++ 2.pas
@@ -0,0 +1,16 @@
+﻿program PositiveAndNegative;
+var
+  s,d:string;
+  c,i,e:integer;
+begin
+  readln(s);
+  d:=d+s;
+  for i:=0 to 1 do
+  begin
+  d:=d+', '+s;
+  end;
+  writeln();
+  writeln(d);
+  c:= Length(d);
+  writeln('Количество символов в строке: ', c);
+end.
